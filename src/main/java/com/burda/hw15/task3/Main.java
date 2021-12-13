920d1ea845d86197f84f84b43d62bca152358ddc
Asternus <malafiko@gmail.com>
2021-12-13 23:30:35 +0200

    add hw 15 fix+

--- src/main/java/com/burda/hw15/task3/Main.java
+++ src/main/java/com/burda/hw15/task3/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    final static List<String> animal = new ArrayList<String>();
+    final static List<String> animal = new ArrayList<>();
 
     public static void main(String[] args) {
         animal.add("Dog");
