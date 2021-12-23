29335cef5234fb40a4f5ffa0554df519788ca836
Asternus <malafiko@gmail.com>
2021-12-23 05:59:10 +0200

    add hw 18

---
+++ src/main/java/com/burda/hw18/Test.java
@@ -0,0 +1,10 @@
+package com.burda.hw18;
+
+public class Test {
+    public static void main(String[] args) {
+        Box boxXml = Xml.readXml("xmlhw.xml");
+        System.out.println("xml : " + boxXml);
+        Box boxJson = Json.readJson("jsonhw.json");
+        System.out.println("json : " + boxJson);
+    }
+}
